78d270c85a7ad9d7a6655d501a35f02679f874f6
abdelazizgamal <abdelazizgamal67@gmail.com>
2026-04-22 08:40:28 +0200

    feat: API Version 1.

--- E-Commerce_APIs/Controllers/ProductsController.cs
+++ E-Commerce_APIs/Controllers/ProductsController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.APIs
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
